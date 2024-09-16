a8721645159c0db72f2242d7b51e8a6be8d0fd0f
Roshan Abhishek <roshan@eximpe.com>
2024-09-16 19:10:40 +0530

    Write streams

---
+++ streams/write-streams/writeMany.js
@@ -0,0 +1,94 @@
+// const fs = require("node:fs/promises");
+
+// Exection time: 9.5s
+// CPU Usage: 101%
+// Memory Usage: 50MB
+// (async () => {
+//     console.time("writeMany")
+//     const fileHandle = await fs.open('write.txt', "w");
+
+//     for(let i=0;i< 1000000;i++){
+//         await fileHandle.write(` ${i} `);
+//     }
+//     console.timeEnd("writeMany")
+// })();
+
+// const fs = require("node:fs");
+
+// Exection time: 1.6s
+// CPU Usage: 100%
+// Memory Usage: 50MB
+// (async () => {
+//     console.time("writeMany")
+//     fs.open('write.txt', "w", (err, fd) => {
+//         for(let i=0;i< 1000000;i++){
+//             fs.writeSync(fd, ` ${i} `);
+//         }
+//         console.timeEnd("writeMany")
+//     });
+// })();
+
+const fs = require("node:fs/promises");
+
+// Exection time: 250ms
+// CPU Usage: 100%
+// Memory Usage: ~150MB
+// (async () => {
+//     console.time("writeMany")
+//     const fileHandle = await fs.open('write.txt', "w");
+//     const stream = fileHandle.createWriteStream();
+
+//     for(let i=0;i< 10000000;i++){
+//         const buf = Buffer.from(` ${i} `, 'utf8');
+//         stream.write(buf);
+//     }
+//     console.timeEnd("writeMany")
+// })();
+
+// INTERNAL BUFFER DEFAULT 16384 Bytes ~16KB
+// on internal_buffer===16384 it write data and empties internal buffer
+
+// For 10 million
+// Exection time: 3secs
+// CPU Usage: 100%
+// Memory Usage: ~150MB
+
+// For 1 million
+// Exection time: ~350ms
+// CPU Usage: 100%
+// Memory Usage: ~130MB
+(async () => {
+    console.time("writeMany")
+    const fileHandle = await fs.open('write.txt', "w");
+    const stream = fileHandle.createWriteStream();
+    const buf = Buffer.alloc(1e+8, 10);
+
+
+    let i = 0;
+    const writeManyFn = () => {
+        while(i<1000000) {
+          const buf = Buffer.from(` ${i} `, 'utf8');
+
+          if(i === 999999) {
+            return stream.end(buf);
+          }
+
+         // return true if internal buffer < highWaterMark
+          if(!stream.write(buf)) break;
+
+          i++;
+        }
+    }
+
+    writeManyFn();
+
+    // listens everytime on internal buffer is emptied
+    stream.on('drain', () => {
+        writeManyFn();
+    });
+    
+    stream.on('finish', () => {
+        console.timeEnd("writeMany")
+        fileHandle.close();
+    });
+})();
